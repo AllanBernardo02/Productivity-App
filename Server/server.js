4ada803c3620a9bb92b7a3f485d1de619a318714
AllanBernardo02 <bernardoallan03@gmail.com>
2023-07-31 10:52:29 +0800

    Done mongoDB connection

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 const port = 5000;
+require("dotenv").config();
+
+const dbConfig = require("./config/dbConfig");
 
 app.get("/", (req, res) => {
   res.send("Hello, this is the root endPointss!");
