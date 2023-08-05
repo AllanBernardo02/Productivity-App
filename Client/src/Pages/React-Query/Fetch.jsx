adaf1df877bf613bfc92922c08676af2cdb91fe6
AllanBernardo02 <bernardoallan03@gmail.com>
2023-08-05 21:19:26 +0800

    Add design fetch page

--- Client/src/Pages/React-Query/Fetch.jsx
+++ Client/src/Pages/React-Query/Fetch.jsx
@@ -46,23 +46,38 @@ const Fetch = () => {
 
   return (
     <>
-      <div>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input placeholder="Input ID" onChange={handleChange} id="id" />
-          <input placeholder="Input Name" onChange={handleChange} id="name" />
-          <input
-            placeholder="Input AlterEgo"
-            onChange={handleChange}
-            id="alterEgo"
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-      <div>
-        <h2>Fetch Using React Query</h2>
-        {data?.map((o, index) => (
-          <h1 key={index}>{o.name}</h1>
-        ))}
+      <div className="container d-flex justify-content-center align-items-center">
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="Input ID"
+              onChange={handleChange}
+              id="id"
+              className="form-control mb-3"
+            />
+            <input
+              placeholder="Input Name"
+              onChange={handleChange}
+              id="name"
+              className="form-control mb-3"
+            />
+            <input
+              placeholder="Input AlterEgo"
+              onChange={handleChange}
+              id="alterEgo"
+              className="form-control mb-3"
+            />
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div>
+          <h2>Fetch Using React Query</h2>
+          {data?.map((o, index) => (
+            <h1 key={index}>{o.name}</h1>
+          ))}
+        </div>
       </div>
     </>
   );
