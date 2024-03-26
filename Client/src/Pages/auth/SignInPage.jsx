b15b1fe24340bcd1557ec1d48cca51c6b69dc4e1
AllanBernardo02 <bernardoallan03@gmail.com>
2024-03-26 18:02:42 +0800

    yon

--- Client/src/Pages/auth/SignInPage.jsx
+++ Client/src/Pages/auth/SignInPage.jsx
@@ -38,6 +38,7 @@ const SignInPage = () => {
         console.log("No response");
         console.log("No response");
         console.log("No response");
+        console.log("No response");
        
       }
     } catch (error) {
